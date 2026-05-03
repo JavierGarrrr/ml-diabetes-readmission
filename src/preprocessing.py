9d853cd01da9c6f935ba7593a3e8c4331f49caa9
Javi <javierener@gmail.com>
2026-05-02 22:12:03 -0700

    stage 2 preproc complete

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -26,7 +26,7 @@ def audit_missing(df):
         })
     report = (
         pd.DataFrame(records)
-        .query("total_missing > 1")
+        .query("total_missing > 0")
         .sort_values("%_missing", ascending=False)
         .reset_index(drop=True)
     )
@@ -49,25 +49,49 @@ def fill_mode(df, col_name):
     df[col_name] = df[col_name].fillna(mode_val)
     return df
 
+def fill_unknown(df, col_name):
+    df = df.copy()
+    df[col_name] = df[col_name].fillna("Unknown")
+    return df
+
 def remove_duplicates(df):
     df = df.sort_values("encounter_id")
     df = df.drop_duplicates("patient_nb", keep = "first")
     return df
 
+def remove_discharge_records(df):
+    removal_ids = [11,13,14,19,20,21]
+    mask = df["discharge_disposition_id"].isin(removal_ids)
+    df = df[~mask]
+    return df
+
 def testing(PATH, MISSING_SYMBOL):
+    print("--------------------------")
+    print("init")
+    print("--------------------------")
     df = load(PATH)
     report = audit_missing(df)
+    print("--------------------------")
+    print("Report initial results")
+    print("--------------------------")
+    print(report)
     df = remove_main(df)
     df = replace_missing(df)
     df = fill_mode(df, "race")
     df = fill_mode(df, "diag_1")
     df = fill_mode(df, "diag_2")
     df = fill_mode(df, "diag_3")
+    df = remove_discharge_records(df)
+    df = fill_unknown(df, "medical_specialty")
     report2 = audit_missing(df)
+    print("--------------------------")
+    print("Report new results after removal")
+    print("--------------------------")
+    print(report2)
     return df
 
 
-df = testing(PATH)
+df = testing(PATH, MISSING_SYMBOL)
 
 
 
